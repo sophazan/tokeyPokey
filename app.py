c3347ffb08b0961f04cd704f4e1a1e1b3ebf7ba2
sophazan <sophie.sepphazan@gmail.com>
2017-09-08 08:29:07 -0700

    pimping requirements to deploy to heroku

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from flask.ext.heroku import Heroku
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/tokeypokey'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/tokeypokey'
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 # Create our database model
